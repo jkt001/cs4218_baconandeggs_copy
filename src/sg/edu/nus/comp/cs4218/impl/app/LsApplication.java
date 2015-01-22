22ac6548bea3d55b0f13a721aa86b04e6daf7511
Ang Civics <angcivics@gmail.com>
2015-01-22 16:59:02 +0800

    test again

--- src/sg/edu/nus/comp/cs4218/impl/app/LsApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/LsApplication.java
@@ -1,5 +1,12 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 public class LsApplication implements Application{
+	/**
+	 * Runs application with specified input data and specified output stream.
+	 */
+	public void run(String[] args, InputStream stdin, OutputStream stdout) 
+			throws AbstractApplicationException{
+		
+	}
 	
 }
