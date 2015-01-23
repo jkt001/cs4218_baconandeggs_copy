db818293a58c9ffbaba5b2269eee60a3c74b43e7
Kai Yao <yky234-github@yahoo.com.sg>
2015-01-23 12:35:00 +0800

    Pipe command

---
+++ src/sg/edu/nus/comp/cs4218/impl/cmd/PipeCommand.java
@@ -0,0 +1,35 @@
+package sg.edu.nus.comp.cs4218.impl.cmd;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import sg.edu.nus.comp.cs4218.Command;
+import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
+import sg.edu.nus.comp.cs4218.exception.ShellException;
+
+public class PipeCommand implements Command {
+
+	@Override
+	public void evaluate(InputStream stdin, OutputStream stdout)
+			throws AbstractApplicationException, ShellException {
+
+		byte[] buffer = new byte[1024];
+		int len;
+		try {
+			while ((len = stdin.read(buffer)) != -1) {
+				stdout.write(buffer, 0, len);
+			}
+		} catch (IOException e) {
+			throw new ShellException("Error in pipe");
+		}
+
+	}
+
+	@Override
+	public void terminate() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
