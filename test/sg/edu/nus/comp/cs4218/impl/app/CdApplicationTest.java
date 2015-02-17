57760e257b833be627d6d2973820de538ade74e5
Kai Yao <yky234-github@yahoo.com.sg>
2015-02-17 17:41:50 +0800

    Added tests for other OS in CdApplicationTest

--- test/sg/edu/nus/comp/cs4218/impl/app/CdApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/CdApplicationTest.java
@@ -3,6 +3,16 @@ package sg.edu.nus.comp.cs4218.impl.app;
 import static org.junit.Assert.*;
 import static org.junit.Assume.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -79,111 +89,222 @@ public class CdApplicationTest {
 	// Test folder that exists, absolute path
 	@Test
 	public void testSimpleAbsolutePathThatExists() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess(getUserDir(), "C:\\", "C:\\");
+		if (isWindows()) {
+			testCdExpectSuccess(getUserDir(), "C:\\", "C:\\");
+		}else{
+			testCdExpectSuccess(getUserDir(), "/", "/");
+		}
 	}
 	
 	// Test folder that exists, absolute path
 	@Test
 	public void testAbsolutePathWithOneFolderThatExists() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess(getUserDir(), "C:\\Windows", "C:\\Windows");
+		if (isWindows()) {
+			testCdExpectSuccess(getUserDir(), "C:\\Windows", "C:\\Windows");
+		}else if (isMac()){
+			testCdExpectSuccess(getUserDir(), "/System", "/System");
+		}else{
+			testCdExpectSuccess(getUserDir(), "/var", "/var");
+		}
 	}
 	
 	// Test folder that exists, absolute path
 	@Test
 	public void testAbsolutePathWithOneFolderWithExtraSlashThatExists() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess(getUserDir(), "C:\\Windows\\", "C:\\Windows");
+		if (isWindows()) {
+			testCdExpectSuccess(getUserDir(), "C:\\Windows\\", "C:\\Windows");
+		}else if (isMac()){
+			testCdExpectSuccess(getUserDir(), "/System/", "/System");
+		}else{
+			testCdExpectSuccess(getUserDir(), "/var/", "/var");
+		}
 	}
 	
 	// Test folder that does not exist, absolute path
 	@Test
-	public void testNonExistentAbsolutePath() {
-		assumeTrue(isWindows());
-		testCdExpectFailure(getUserDir(), "C:\\InvalidDir");
+	public void testNonExistentAbsolutePath() {		
+		if (isWindows()) {
+			testCdExpectFailure(getUserDir(), "C:\\InvalidDir");
+		}else{
+			testCdExpectSuccess(getUserDir(), "/BlaaBlaaBlaa", "/BlaaBlaaBlaa");
+		}
 	}
 	
 	// Test folder that exists, relative subdirectory
 	@Test
 	public void testRelativePathToParent() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\Windows", "..", "C:\\");
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\Windows", "..", "C:\\");
+		}else if (isMac()){
+			testCdExpectSuccess("/System/", "..", "/");
+		}else{
+			testCdExpectSuccess("/var/", "..", "/");
+		}
 	}
 	
 	// Test folder that exists, relative subdirectory
 	@Test
 	public void testRelativePathUpTwoLevels() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\Windows\\System32", "..\\..", "C:\\");
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\Windows\\System32", "..\\..", "C:\\");
+		}else if (isMac()){
+			testCdExpectSuccess("/System/Library", "../..", "/");
+		}else{
+			testCdExpectSuccess("/var/tmp", "../..", "/");
+		}
 	}
 	
 	// Test folder that exists, relative subdirectory
 	@Test
 	public void testRelativePathToParentAndSubdirectory() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\Windows\\System32", "..\\System", "C:\\Windows\\System");
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\Windows\\System32", "..\\System", "C:\\Windows\\System");
+		}else if (isMac()){
+			testCdExpectSuccess("/System/Library", "../..", "/"); //TODO: Blaa blaa
+		}else{
+			testCdExpectSuccess("/var/tmp", "../log", "/var/log");
+		}
 	}
 	
 	// Test folder that exists, root-relative subdirectory
 	@Test
-	public void testRootRelativePath() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\Windows", "\\", "C:\\");
+	public void testRootRelativePath() {		
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\Windows", "\\", "C:\\");
+		}else if (isMac()){
+			testCdExpectSuccess("/System", "/", "/");
+		}else{
+			testCdExpectSuccess("/var", "/", "/");
+		}
 	}
 	
 	// Test folder that exists, root-relative subdirectory
 	@Test
-	public void testRootRelativePathWithSubdirectory() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\Windows", "\\Users", "C:\\Users");
+	public void testRootRelativePathWithSubdirectory() {		
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\Windows", "\\Users", "C:\\Users");
+		}else if (isMac()){
+			testCdExpectSuccess("/System", "/Applications", "/Applications");
+		}else{
+			testCdExpectSuccess("/var", "/usr", "/usr");
+		}
 	}
 	
 	// Test cd to beyond root of drive
 	@Test
-	public void testRelativePathBeyondRoot() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\", "..", "C:\\");
+	public void testRelativePathBeyondRoot() {		
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\", "..", "C:\\");
+		}else{
+			testCdExpectSuccess("/", "..", "/");
+		}
 	}
 	
 	// Test cd to beyond root of drive
 	@Test
 	public void testRelativePathBeyondRootByGoingUpTwoLevels() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\Windows", "..\\..", "C:\\");
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\Windows", "..\\..", "C:\\");
+		}else if (isMac()){
+			testCdExpectSuccess("/System", "../..", "/");
+		}else{
+			testCdExpectSuccess("/var", "../..", "/");
+		}
 	}
 	
 	// Test cd to beyond root of drive
 	@Test
 	public void testRelativePathBeyondRootByRootAndParent() {
-		assumeTrue(isWindows());
-		testCdExpectSuccess("C:\\Windows", "\\..", "C:\\");
+		if (isWindows()) {
+			testCdExpectSuccess("C:\\Windows", "\\..", "C:\\");
+		}else if (isMac()){
+			testCdExpectSuccess("/System", "/..", "/");
+		}else{
+			testCdExpectSuccess("/var", "/..", "/");
+		}
 	}
 	
 	@Test
 	public void testRunCdDirWithNoPermissionsToCdTo() {
-		assumeTrue(isWindows());
-		CdApplication app = new CdApplication();
-		String[] params = {"C:\\System Volume Information"};
-		try {
-			app.run(params, System.in, System.out);
-		} catch (CdException e) {
-			System.out.println(e.getMessage());
+				
+		if (isWindows()){
+			CdApplication app = new CdApplication();
+			String[] params = {"C:\\System Volume Information"};
+			try {
+				app.run(params, System.in, System.out);
+			} catch (CdException e) {
+				System.out.println(e.getMessage());
+			}
+			testCdExpectFailure("C:\\", "C:\\System Volume Information");
+		}else{
+			// Create directory with no permissions to CD into
+			// in POSIX operating systems this is denoted by the
+			// lack of execute bit on that directory
+			
+			String tempFolderName = "testCdApplicationTempDir";
+			Path tempFolderPath = FileSystems.getDefault().getPath(System.getProperty("user.dir"), tempFolderName);
+			
+			Set<PosixFilePermission> perms =
+				    PosixFilePermissions.fromString("rw-rw----");
+				FileAttribute<Set<PosixFilePermission>> attributes =
+				    PosixFilePermissions.asFileAttribute(perms);
+			try {
+				Files.createDirectory(tempFolderPath, attributes);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			testCdExpectFailure(".", tempFolderPath.normalize().toString());
+			
+			try {
+				Files.deleteIfExists(tempFolderPath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		testCdExpectFailure("C:\\", "C:\\System Volume Information");
 	}
 	
 	@Test
 	public void testDirectoryWithNoPermissionsToListContentsOf() {
-		assumeTrue(isWindows());
-		CdApplication app = new CdApplication();
-		String[] params = {"C:\\$RECYCLE.BIN"};
-		try {
-			app.run(params, System.in, System.out);
-		} catch (CdException e) {
-			System.out.println(e.getMessage());
+		if (isWindows()){
+			CdApplication app = new CdApplication();
+			String[] params = {"C:\\$RECYCLE.BIN"};
+			try {
+				app.run(params, System.in, System.out);
+			} catch (CdException e) {
+				System.out.println(e.getMessage());
+			}
+			testCdExpectSuccess("C:\\", "C:\\$RECYCLE.BIN", "C:\\$RECYCLE.BIN");
+		}else{
+			// Create directory with no permissions to list contents of
+			// in POSIX operating systems this is denoted by the lack of 
+			// read bit on that directory
+			
+			String tempFolderName = "testCdApplicationTempDir";
+			Path tempFolderPath = FileSystems.getDefault().getPath(System.getProperty("user.dir"), tempFolderName);
+			
+			Set<PosixFilePermission> perms =
+				    PosixFilePermissions.fromString("-w--w----");
+				FileAttribute<Set<PosixFilePermission>> attributes =
+				    PosixFilePermissions.asFileAttribute(perms);
+			try {
+				Files.createDirectory(tempFolderPath, attributes);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			testCdExpectSuccess(".", tempFolderPath.normalize().toString(), tempFolderPath.normalize().toString());
+			
+			try {
+				Files.deleteIfExists(tempFolderPath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		testCdExpectSuccess("C:\\", "C:\\$RECYCLE.BIN", "C:\\$RECYCLE.BIN");		
 	}
 
 	private void testCdExpectFailure(String initialDirectory, String cdPath) {
@@ -218,6 +339,20 @@ public class CdApplicationTest {
 	
 	public boolean isWindows(){
 		if (System.getProperty("os.name").startsWith("Windows")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isMac(){
+		if (System.getProperty("os.name").startsWith("Mac OS X")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isLinux(){
+		if (System.getProperty("os.name").startsWith("Linux")) {
 			return true;
 		}
 		return false;
