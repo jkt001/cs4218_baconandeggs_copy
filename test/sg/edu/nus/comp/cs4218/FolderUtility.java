bbc892a5d1b29258bf07eda9db9b2697cf02c415
Kai Yao <yky234-github@yahoo.com.sg>
2015-02-24 05:01:04 +0800

    More Javadoc comments

--- test/sg/edu/nus/comp/cs4218/FolderUtility.java
+++ test/sg/edu/nus/comp/cs4218/FolderUtility.java
@@ -8,6 +8,51 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
+/**
+ * The FolderUtility class makes it easy to generate and delete folders for
+ * testing purposes.
+ * 
+ * <p>
+ * When testing, there is sometimes the need to generate a reasonably complex
+ * folder structure to test the behavior of the application. However, manually
+ * creating folders, then subfolders, and their contents, can be very
+ * long-winded in Java. This class aims to simplify such tasks.
+ * </p>
+ * 
+ * <p>
+ * For example, to generate a folder structure like the following:
+ * 
+ * <pre>
+ * +--F1
+ * |  +--a.txt
+ * |  \--b.txt
+ * +--F2
+ * |  +--F2a
+ *    |  \--c.txt
+ * |  +--d.txt
+ * |  \--e.txt
+ * \--F3
+ * </pre>
+ * 
+ * can be easily done with the following code:
+ * 
+ * <pre>
+ * FolderUtility.generate("testFolder", 
+ *     new TestFolder("F1",
+ *         new TestFile("a.txt", "dataA"),
+ *         new TestFile("b.txt", "dataB"),
+ *     ),
+ *     new TestFolder("F2",
+ *         new TestFolder("F2a",
+ *             new TestFile("c.txt", "dataC")
+ *         ),
+ *         new TestFile("d.txt", "dataD"),
+ *         new TestFile("e.txt", "dataE"),
+ *     ),
+ *     new TestFolder("F3")
+ * );
+ * </pre>
+ */
 public final class FolderUtility {
 	
 	private FolderUtility(){
@@ -17,11 +62,23 @@ public final class FolderUtility {
 	public interface TestEntry {
 		public String getName(); 
 	};
-	
+
+	/**
+	 * The TestFolder class represents a folder for the generation method.
+	 */
 	public static class TestFolder implements TestEntry {
 		String name;
 		TestEntry[] contents;
 		
+		/**
+		 *  Creates a new TestFolder.
+		 * 
+		 * @param name
+		 *            The name of the folder.
+		 * @param contents
+		 *            One or more TestFolder(s) or TestFile(s) to be placed in
+		 *            this folder.
+		 */
 		public TestFolder(String name, TestEntry... contents) {
 			super();
 			this.name = name;
@@ -39,11 +96,22 @@ public final class FolderUtility {
 		
 	}
 	
+	/**
+	 * The TestFile class represents a folder for the generation method.
+	 */
 	public static class TestFile implements TestEntry {
 		
 		String name;
 		String data;
 		
+		/**
+		 * Creates a new TestFile.
+		 * 
+		 * @param name
+		 *            The name of the file.
+		 * @param data
+		 *            The string that will be written into the file.
+		 */
 		public TestFile(String name, String data) {
 			super();
 			this.name = name;
@@ -61,6 +129,17 @@ public final class FolderUtility {
 		
 	}
 	
+	/**
+	 * Generates a folder structure based on the contents provided.
+	 * 
+	 * @param	path
+	 * 			A path whereby the folder structure will be generated in.
+	 * @param	contents
+	 * 			One or more TestFile(s) or TestFolder(s) that will be generated.
+	 * 
+	 * @throws	IOException
+	 * 			If an I/O exception occurs when generating the folder structure.
+	 */
 	public static void generate(String path, TestEntry... contents) throws IOException{
 		for (int i = 0; i < contents.length; i++) {
 			TestEntry entry = contents[i];
@@ -79,6 +158,14 @@ public final class FolderUtility {
 		}
 	}
 	
+	/**
+	 * Deletes a folder and all its contents.
+	 * 
+	 * @param	path
+	 * 			The path to the folder to delete.
+	 * @throws	IOException
+	 * 			If an I/O exception occurs when deleting the folder.
+	 */
 	public static void delete(String path) throws IOException{
 		Path pathToDelete = Paths.get(path);
 		Files.walkFileTree(pathToDelete, new SimpleFileVisitor<Path>() {
--- test/sg/edu/nus/comp/cs4218/OSCheck.java
+++ test/sg/edu/nus/comp/cs4218/OSCheck.java
@@ -1,11 +1,19 @@
 package sg.edu.nus.comp.cs4218;
 
+/**
+ * The OSCheck utility class implements some helper methods for determining the
+ * Operating System that the user is currently executing.
+ */
 public final class OSCheck {
-	
-	private OSCheck(){
+
+	private OSCheck() {
 		// Don't need to have constructor for this utility class
 	}
 	
+	/**
+	 * Checks if the current OS is Windows
+	 * @return true if the OS is Windows, false otherwise
+	 */
 	public static boolean isWindows(){
 		if (System.getProperty("os.name").startsWith("Windows")) {
 			return true;
@@ -13,6 +21,10 @@ public final class OSCheck {
 		return false;
 	}
 	
+	/**
+	 * Checks if the current OS is Mac OS X
+	 * @return true if the OS is Mac OS X, false otherwise
+	 */
 	public static boolean isMac(){
 		if (System.getProperty("os.name").startsWith("Mac OS X")) {
 			return true;
@@ -20,6 +32,10 @@ public final class OSCheck {
 		return false;
 	}
 	
+	/**
+	 * Checks if the current OS is Linux
+	 * @return true if the OS is Linux, false otherwise
+	 */
 	public static boolean isLinux(){
 		if (System.getProperty("os.name").startsWith("Linux")) {
 			return true;
