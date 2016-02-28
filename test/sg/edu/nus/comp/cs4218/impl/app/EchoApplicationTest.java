439cdea4ecc757c7c2a266e3e5333592bd10d646
Wilson Tan <weeweedry@live.com>
2016-02-28 17:14:05 +0800

    Completed echo bugfixing and testing

--- test/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
@@ -22,18 +22,56 @@ import org.junit.rules.ExpectedException;
 
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.CatException;
+import sg.edu.nus.comp.cs4218.exception.EchoException;
 
 public class EchoApplicationTest {
 	
-	private static CatApplication catApplication;
-	private static InputStream inStream;
-	
+	private EchoApplication echoApp;
+	private InputStream inStream;
+
 	private OutputStream outStream;
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 	
 	@Before
-	public void setup() throws IOException {
+	public void setup() {
+		echoApp = new EchoApplication();
+		byte[] dummy = {};
+		inStream = new ByteArrayInputStream(dummy); 
 		outStream = new ByteArrayOutputStream();
-		inStream.reset();
 	}
 	
+	@Test
+	public void shouldPrintEmptyLine() throws EchoException {
+		String [] args = {};
+		echoApp.run(args, inStream, outStream);
+		
+		assertEquals("\n", outStream.toString());
+	}
+
+	@Test
+	public void shouldPrintLine() throws EchoException {
+		String [] args = {"arg1", "arg2"};
+		echoApp.run(args, inStream, outStream);
+		
+		assertEquals("arg1 arg2\n", outStream.toString());
+	}
+
+	@Test
+	public void shouldThrowExceptionForNullArgs() throws EchoException {
+		thrown.expect(EchoException.class);
+		thrown.expectMessage("Null arguments");
+
+		echoApp.run(null, inStream, outStream);
+	}
+
+	@Test
+	public void shouldThrowExceptionForNullStdOut() throws EchoException {
+		thrown.expect(EchoException.class);
+		thrown.expectMessage("OutputStream not provided");
+
+		String [] args = {"arg1", "arg2"};
+
+		echoApp.run(args, inStream, null);
+	}
 }
